f84a2da79fdbe6aaf1d597f08fe665acbb17d102
Rifk1 <rifkimiftah04@gmail.com>
2022-05-28 11:06:54 +0700

    18a
    awal

---
+++ app/src/main/java/com/example/recylerviewb/AddMahasiswaMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.recylerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMahasiswaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa_main);
+    }
+}
